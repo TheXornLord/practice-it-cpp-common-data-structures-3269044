799c9e141ce21af18c8f73d30a1d279a4f6dec50
TheXornLord <146850104+TheXornLord@users.noreply.github.com>
2023-12-14 09:20:03 -0500

    Update sum-array.cpp

--- src/Ch01-Arrays/01_02/end/sum-array.cpp
+++ src/Ch01-Arrays/01_02/end/sum-array.cpp
@@ -3,21 +3,19 @@ using namespace std;
 
 int calculateSum(const int arr[], size_t size)
 {
-    int sum;
+    int sum = 0;
 
-    for (int i = 0; i < size_t; i++)
+    for (int i = 0; i < size; i++)
     {
 
         sum += arr[i];
-
     }
 
-    cout << sum; 
-
+    return sum;
 }
 
 int main()
 {
-    const int ints[]{ 1, -7, 17 };
+    const int ints[]{1, -7, 17};
     cout << "The sum of all elements in the array is " << calculateSum(ints, 3) << endl;
 }
